df24c6fa1ca1cbd71519195eede8bc3f935cc446
YuSunjo <tnswh2023@gmail.com>
2020-08-24 20:07:21 +0900

    mongoDB confidential information protection

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const port = 5000
 const bodyParser = require('body-parser')
 const {User} = require('./models/User');
 
+const config = require('./config/key')
 
 app.use(bodyParser.urlencoded({extended :true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://root:147asdfg@cluster0.caluy.mongodb.net/Cluster0?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 }).then(()=> console.log('mongodb connected'))
   .catch(err => console.log(err))
