dc1bb95a1d56a0572b47f7961e5048535873d497
YuSunjo <tnswh2023@gmail.com>
2020-08-24 15:29:23 +0900

    create route /register  sign up result

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express= require('express');
 const  app =express()
 const port = 5000
+const bodyParser = require('body-parser')
+const {User} = require('./models/User');
 
 
+app.use(bodyParser.urlencoded({extended :true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://root:147asdfg@cluster0.caluy.mongodb.net/Cluster0?retryWrites=true&w=majority',{
@@ -12,8 +16,20 @@ mongoose.connect('mongodb+srv://root:147asdfg@cluster0.caluy.mongodb.net/Cluster
 
 
 app.get('/', (req,res) => {
-    res.send('hello');
+    res.send('hello hi');
 })
 
+app.post('/register', (req, res) => {
+    //회원가입할때 필요한 정보들을 client에서 가져오면 그것을 데이터베이스에 넣어줌
+    
+    const user = new User(req.body)
+
+    user.save((err,userInfo) => {
+        if(err) return res.json({success: false,err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port , () => console.log(`example app${port}`))
