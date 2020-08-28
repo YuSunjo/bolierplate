eb43310ca1b83c11a776a25caab4cb4e438af422
YuSunjo <tnswh2023@gmail.com>
2020-08-28 14:37:42 +0900

    implement registerPage (sign up)

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,11 +1,71 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { withRouter } from 'react-router-dom';
+import {useDispatch} from 'react-redux'
+import {registerUser} from '../../../_actions/user_actions'
 
-function RegisterPage() {
+function RegisterPage(props) {
+    const dispatch = useDispatch();
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+    const [Name, setName] = useState("")
+    const [ConfirmPassword, setConfirmPassword] = useState("")
+
+    const onEmailHandler =(event) => {
+        setEmail(event.currentTarget.value)
+    }
+    const onNameHandler =(event) => {
+        setName(event.currentTarget.value)
+    }
+    const onPasswordHandler =(event) => {
+        setPassword(event.currentTarget.value)
+    }
+    const onConfirmPasswordHandler =(event) => {
+        setConfirmPassword(event.currentTarget.value)
+    }
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+       
+        if(Password !==ConfirmPassword){
+            return alert('differnt password and ConfirmPassword. Change equal it')
+        }
+
+        let body={
+            email:Email,
+            password:Password,
+            name:Name
+        }
+        dispatch(registerUser(body))
+        .then(response=> {
+            if(response.payload.success){
+                props.history.push('/login')
+            }else {
+                alert('fail to sign up')
+            }
+        })
+
+    }
+
+
     return (
-        <div>
-            RegisterPage
+        <div style={{display:'flex', justifyContent: 'center', alignItems:"center", width:"100%",height:"100vh"}}>
+            <form style={{display: 'flex', flexDirection:"column"}}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>Name</label>
+                <input type="text" value={Name} onChange={onNameHandler} />
+                <label>password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+                 <label>ConfirmPassword</label>
+                <input type="password" value={ConfirmPassword} onChange={onConfirmPasswordHandler} />
+                <br />
+                <button type="submit">
+                    sign up
+                </button>
+            </form>
         </div>
     )
 }
 
-export default RegisterPage
+export default withRouter(RegisterPage)
