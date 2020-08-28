7ed71165e2b9dccf5a182b9d67fb308b57f13563
YuSunjo <tnswh2023@gmail.com>
2020-08-28 14:49:13 +0900

    logout function

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,14 +4,32 @@ import axios from 'axios'
 
 
 
-function LandingPage() {
+
+function LandingPage(props) {
     useEffect(() => {
         axios.get('/api/hello')
         .then(response=> console.log(response.data))
     }, [])
+
+
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+        .then(response => {
+            if(response.data.success) {
+                props.history.push('/login')
+            }else{
+                alert('fail logout')
+            }
+            // console.log(response.data)
+        })
+    }
     return (
         <div style={{display:'flex', justifyContent: 'center', alignItems:"center", width:"100%",height:"100vh"}}>
             <h2>LandingPage</h2>
+
+            <button onClick={onClickHandler}>
+                logout
+            </button>
         </div>
     )
 }
